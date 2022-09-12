a5995b8604bea3344260b2b4db8ddcc424e5693d
Jairgun <jasda.jair9un@mail.kmutt.ac.th>
2022-09-12 12:16:19 +0700

    test

--- getMaxLengthString.js
+++ getMaxLengthString.js
@@ -2,5 +2,18 @@ const { template } = require('@babel/core')
 
 function getMaxLengthString(arrayOfString) {
   //code here
+  if(arrayOfString == undefined || null) return undefined
+  else {
+    max_str = arrayOfString[0].length
+    ans = arrayOfString[0]
+    for (i=1; i<arrayOfString.length; i++) {
+      var maxi = arrayOfString[i].length
+      if (maxi > max_str) {
+        ans = arra[i]
+        max_str = maxi
+      }
+    }
+  }
+  return ans
 }
 module.exports = getMaxLengthString
